86d766e94d5ec1226412dd9715da73e3d1067375
Степанов Эдуард <49841766+stepanoff21@users.noreply.github.com>
2020-02-21 11:01:14 +0300

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
-
-
 'use strict';
+
+const DAY_STRING = ['день', 'дня', 'дней'];
 
 const DATA = {
   whichSite: ['landing', 'multiPage', 'onlineStore'],
@@ -24,9 +24,25 @@ const startButton = document.querySelector('.start-button'),
   total = document.querySelector('.total'),
   fastRange = document.querySelector('.fast-range'),
   totalPriceSum = document.querySelector('.total_price__sum'),
-  adapt = document.querySelector('#adapt'),
-  mobileTemplates = document.querySelector('#mobileTemplates');
+  adapt = document.getElementById('adapt'),
+  mobileTemplates = document.getElementById('mobileTemplates'),
+  typeSite = document.querySelector('.type-site'),
+  maxDeadline = document.querySelector('.max-deadline'),
+  rangeDeadline = document.querySelector('.range-deadline'),
+  deadlineValue = document.querySelector('.deadline-value'),
+  desktopTemplatesCheckBox = document.getElementById('desktopTemplates'),
+  adaptCheckBox = document.getElementById('adapt'),
+  mobileTemplatesCheckBox = document.getElementById('mobileTemplates'),
+  editableCheckBox = document.getElementById('editable'),
+  desktopTemplatesValue = document.querySelector('.desktopTemplates_value'),
+  adaptValue = document.querySelector('.adapt_value'),
+  mobileTemplatesValue = document.querySelector('.mobileTemplates_value'),
+  editableValue = document.querySelector('.editable_value');
 
+function declOfNum(n, titles) {
+  return n + ' ' + titles[n % 10 === 1 && n % 100 !== 11 ?
+    0 : n % 10 >= 2 && n % 10 <= 4 && (n % 100 < 10 || n % 100 >= 20) ? 1 : 2];
+}
 
 function showElem(elem) {
   elem.style.display = 'block';
@@ -36,10 +52,22 @@ function hideElem(elem) {
   elem.style.display = 'none';
 }
 
+function renderTextContent(total, site, maxDay, minDay) {
+  totalPriceSum.textContent = total;
+  typeSite.textContent = site;
+  maxDeadline.textContent = declOfNum(maxDay, DAY_STRING);
+  rangeDeadline.min = minDay;
+  rangeDeadline.max = maxDay;
+  deadlineValue.textContent = declOfNum(rangeDeadline.value, DAY_STRING);
+}
+
 function priceCalculation(elem) {
   let result = 0,
       index = 0,
-      options = [];
+      options = [],
+      site = '',
+      maxDeadlineDay = DATA.deadlineDay[index][1],
+      minDeadlineDay = DATA.deadlineDay[index][0];
 
 
   if (elem.name === 'whichSite') {
@@ -50,11 +78,13 @@ function priceCalculation(elem) {
     }
     hideElem(fastRange);
   }
-  
 
   for (const item of formCalculate.elements) {
     if (item.name === 'whichSite' && item.checked) {
       index = DATA.whichSite.indexOf(item.value);
+      site = item.dataset.site;
+      maxDeadlineDay = DATA.deadlineDay[index][1];
+      minDeadlineDay = DATA.deadlineDay[index][0];
     } else if (item.classList.contains('calc-handler') && item.checked) {
       options.push(item.value);
     }
@@ -78,11 +108,23 @@ function priceCalculation(elem) {
 
   result += DATA.price[index];
 
-  totalPriceSum.textContent = result;
+  renderTextContent(result, site, maxDeadlineDay, minDeadlineDay);
 }
 
 function handlerCallBackForm(event) {
   const target = event.target;
+
+  if (adapt.checked) {
+    mobileTemplates.disabled = false;
+  } else {
+    mobileTemplates.disabled = true;
+    mobileTemplates.checked = false;
+  }
+
+  desktopTemplatesValue.textContent = desktopTemplatesCheckBox.checked ? 'Да' : 'Нет';
+  adaptValue.textContent = adaptCheckBox.checked ? 'Да' : 'Нет';
+  mobileTemplatesValue.textContent = mobileTemplatesCheckBox.checked ? 'Да' : 'Нет';
+  editableValue.textContent = editableCheckBox.checked ? 'Да' : 'Нет';  
 
   if (target.classList.contains('want-faster')) {
     target.checked ? showElem(fastRange) : hideElem(fastRange);
@@ -90,13 +132,6 @@ function handlerCallBackForm(event) {
 
   if (target.classList.contains('calc-handler')) {
     priceCalculation(target);
-  }
-
-  if (adapt.checked) {
-    mobileTemplates.disabled = false;
-  } else {
-    mobileTemplates.disabled = true;
-    mobileTemplates.checked = false;
   }
 }
 
